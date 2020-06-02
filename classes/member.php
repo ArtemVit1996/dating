05fa710f742f87c1d4cce08a4c3630e3f3dcdb8b
Artem Vityuk <avityuk@mail.greenriver.edu>
2020-06-01 21:20:08 -0400

    member class and updated composer

--- classes/member.php
+++ classes/member.php
@@ -13,26 +13,122 @@ class Member
     private $_seeking;
     private $_bio;
 
-    /** Default constructor */
-    public function __construct()
+    /** Default constructor
+     * @param string $fname for first name
+     * @param string $lname for last
+     * @param int $age for age
+     * @param string $gender for gender
+     * @param string $phone for phone
+     * @param string $email for email
+     * @param string $state for state
+     * @param string $seeking for seeking
+     * @param string $bio for bio
+     */
+    public function __construct($fname = "", $lname = "", $age = 0,
+                                $gender = "", $phone = "", $email = "", $state = "",
+                                $seeking = "", $bio = "")
     {
-        $this->_fname = "";
-        $this->_lname = "";
-        $this->_age = 0;
-        $this->_gender = "";
-        $this->_phone = "";
-        $this->_email = "";
-        $this->_state = "";
-        $this->_seeking = "";
-        $this->_bio = "";
+        $this->_fname = $fname;
+        $this->_lname = $lname;
+        $this->_age = $age;
+        $this->_gender = $gender;
+        $this->_phone = $phone;
+        $this->_email = $email;
+        $this->_state = $state;
+        $this->_seeking = $seeking;
+        $this->_bio = $bio;
     }
 
 
     /** toString() returns a String representation
      * of a member object
-     * @return string */
+     * @return string
+     */
     public function toString()
     {
+        $string = $this->_fname . ", ";
+        $string .= $this->_lname . ", ";
+        $string .= $this->_age;
+        return $string;
+    }
+
+    // ******** Setters **********
+    public function setFname($fname)
+    {
+        $this->_fname = $fname;
+    }
+    public function setLname($lname)
+    {
+        $this->_lname = $lname;
+    }
+    public function setAge($age)
+    {
+        $this->_age = $age;
+    }
+    public function setGender($gender)
+    {
+        $this->_gender = $gender;
+    }
+    public function setPhone($phone)
+    {
+        $this->_phone = $phone;
+    }
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+    public function setState($state)
+    {
+        $this->_state = $state;
+    }
+    public function setSeeking($seeking)
+    {
+        $this->_seeking = $seeking;
+    }
+    public function setBio($bio)
+    {
+        $this->_bio = $bio;
+    }
+
+    // ********** Getters *********
+    public function getFname() {
+        return $this->_fname;
+    }
+    public function getLname() {
+        return $this->_lname;
+    }
+    public function getAge() {
         return $this->_age;
     }
+    public function getGender() {
+        return $this->_gender;
+    }
+    public function getPhone() {
+        return $this->_phone;
+    }
+    public function getEmail() {
+        return $this->_email;
+    }
+    public function getState() {
+        return $this->_state;
+    }
+    public function getSeeking() {
+        return $this->_seeking;
+    }
+    public function getBio() {
+        return $this->_bio;
+    }
+
+
+
+
+
 }
+
+// testing the class
+//$member = new Member("artem", "vit");
+//echo $member->toString();
+$member2 = new Member("artem", "vit");
+$member2->setGender("Maaan");
+$member2->setAge(44);
+echo $member2->toString();
