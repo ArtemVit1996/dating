1c86d93c84bd51200db2d2ddd862778822c17bd9
Artem Vityuk <avityuk@mail.greenriver.edu>
2020-06-03 14:59:56 -0400

    controller class and validator class added. index page uses the controller class

--- index.php
+++ index.php
@@ -12,188 +12,38 @@ error_reporting(E_ALL);
 
 // Require the autoload file
 require_once("vendor/autoload.php");
-// require the validation file
-require_once("model/validation.php");
-//require_once("classes/Regular.php");
 
 // start a session (must be after the autoload file)
 session_start();
 
-
-// Instantiate the F3 Base class
+// Instantiate my classes
 $f3 = Base::instance();
+$validator = new Validate();
+$controller = new Controller($f3);
 
 // Default route
 $f3->route('GET /', function() {
-    // echo '<h1>Dating website</h1>';
-
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 // page1 route
 $f3->route('GET|POST /page1', function($f3) {
-
-    // If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        # Check if user has opted out for premium membership
-        if (!empty($_POST['membership'])) {
-            $member = new Premium();
-            echo "premium";
-
-        }
-        else {
-            $member = new Regular();
-            echo "regular <br>";
-        }
-
-
-        $isValid = true;
-        if (!validFName($_POST['fname'], $f3)) {
-            $isValid = false;
-        }
-        if (!validLName($_POST['lname'], $f3)) {
-            $isValid = false;
-        }
-        if (!validAge($_POST['age'], $f3)) {
-            $isValid = false;
-        }
-        if (!validPhone($_POST['phone'], $f3)) {
-            $isValid = false;
-        }
-
-        if(isset($_POST['gender'])) {
-            $f3->set("gender", $_POST['gender']);
-        }
-
-        if ($isValid) {
-            # store all the valid fields in the member
-            $member->setFname($_POST['fname']);
-            $member->setLname($_POST['lname']);
-            $member->setAge($_POST['age']);
-            $member->setPhone($_POST['phone']);
-            $member->setGender($_POST['gender']);
-            # put the member into a session
-            $_SESSION['member'] = $member;
-            //echo $_SESSION['member'];
-            //var_dump($_SESSION);
-            # route to the next page (page2)
-            $f3-> reroute('page2');
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/page1.html');
+    $GLOBALS['controller']->page1();
 });
 
 // page2(profile) route
 $f3->route('GET|POST /page2', function($f3) {
-    //echo $_SESSION['member']->toString();
-
-    // If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        # Store not required fields in the session
-        //$_SESSION['email'] = $_POST['email'];
-        //$_SESSION['loc'] = $_POST['loc'];
-        //$_SESSION['seeking'] = $_POST['gender'];
-        //$_SESSION['bio'] = $_POST['bio'];
-
-        # Set f3 variables to session variables to make items sticky
-        $f3->set("loc", $_POST['loc']);
-        $f3->set("bio", $_POST['bio']);
-        $f3->set("seeking",$_POST['gender']);
-
-        $isValid = true;
-        if (!validEmail($_POST['email'], $f3)) {
-            $isValid = false;
-        }
-
-        if ($isValid) {
-            # set the user inputs into the member class(located in the session)
-            $theMember = $_SESSION['member'];
-            $theMember->setEmail($_POST['email']);
-            $theMember->setState($_POST['loc']);
-            $theMember->setSeeking($_POST['gender']);
-            $theMember->setBio($_POST['bio']);
-
-            // check to see if the class is Premium or Regular //method_exists ($theMember , 'setIndoor'
-            if ($theMember instanceof Premium) {
-                // **** Premium ******
-
-                // route to Interest page
-                $f3-> reroute('page3');
-                //echo $theMember;
-                //echo "im premium";
-            }
-            else {
-                // **** Regular ****
-
-                // route to the summary page
-                $f3-> reroute('summary');
-                //echo $theMember;
-                //echo "im regular";
-            }
-
-
-        }
-    }
-    $view = new Template();
-    echo $view->render('views/page2.html');
+    $GLOBALS['controller']->page2();
 });
 
 // page3(interests) route
 $f3->route('GET|POST /page3', function($f3) {
-    // grab the member class stored in a session
-    $theMember = $_SESSION['member'];
-
-    # array if valid outdoor activities
-    $outdoor = array('Hiking', 'Biking', 'Snowboarding', 'Skiing',
-        'Running', 'Roadtrips');
-    # set an f3 outdoor array
-    $f3->set('outdoor', $outdoor);
-
-
-    # array of valid indoor activities
-    $indoor = array('Game night', 'Movie night', 'Baking/cooking',
-        'Video games', 'Writing', 'Reading');
-    # set an f3 indoor array
-    $f3->set('indoor', $indoor);
-    //echo var_dump($indoor);
-
-    // If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        $isValid = true;
-
-        if (!validOutdoor($_POST['outdoor'], $f3, $outdoor, $theMember)) {
-            $isValid = false;
-        }
-
-        if (!validIndoor($_POST['indoor'], $f3, $indoor, $theMember)) {
-            $isValid = false;
-        }
-
-        if ($isValid) {
-            // route to the summary page
-            $f3-> reroute('summary');
-        }
-
-    }
-
-    $view = new Template();
-    echo $view->render('views/page3.html');
+    $GLOBALS['controller']->page3();
 });
 
 // summary route
 $f3->route('GET /summary', function() {
-
-    //session_destroy();
-
-    $view = new Template();
-    echo $view->render('views/summary.html');
-    session_destroy();
+    $GLOBALS['controller']->summary();
 });
 
 // Run F3
--- model/validation.php
+++ model/validation.php
@@ -1,145 +1,157 @@
 <?php
-# Function to validate the first name
-function validFName($fname, $f3) {
-    if (!empty($fname)) {
-        # fname in not empty so check if its all alphabetic
-        if (ctype_alpha($fname)) {
-            # set a session variable to the input
-            //$_SESSION['fname'] = $fname;
-            # set a f3 variable to the input
-            $f3->set("fname", $fname);
-            return true;
-        } # input is not alphabetic, set an f3 to appropriate error
+
+/**
+ * Class Validate that contains methods
+ * that validate user give data
+ * @author Artem Vityuk
+ * @version 1.0
+ */
+class Validate
+{
+    # Function to validate the first name
+    function validFName($fname, $f3) {
+        if (!empty($fname)) {
+            # fname in not empty so check if its all alphabetic
+            if (ctype_alpha($fname)) {
+                # set a session variable to the input
+                //$_SESSION['fname'] = $fname;
+                # set a f3 variable to the input
+                $f3->set("fname", $fname);
+                return true;
+            } # input is not alphabetic, set an f3 to appropriate error
+            else {
+                $f3->set("errors['fname']", "Fist name must be alphabetic");
+            }
+        } # input is empty, set an f3 variable to appropriate error
         else {
-            $f3->set("errors['fname']", "Fist name must be alphabetic");
+            $f3->set("errors['fname']", "First name is required");
         }
-    } # input is empty, set an f3 variable to appropriate error
-    else {
-        $f3->set("errors['fname']", "First name is required");
     }
-}
 
 # Function to validate the last name
-function validLName($lname, $f3) {
-    if (!empty($lname)) {
-        # lname in not empty so check if its all alphabetic
-        if (ctype_alpha($lname)) {
-            # set a session variable to the input
-            //$_SESSION['lname'] = $lname;
-            # set a f3 variable to the input
-            $f3->set("lname", $lname);
-            return true;
-        } # input is not alphabetic, set an f3 to appropriate error
+    function validLName($lname, $f3) {
+        if (!empty($lname)) {
+            # lname in not empty so check if its all alphabetic
+            if (ctype_alpha($lname)) {
+                # set a session variable to the input
+                //$_SESSION['lname'] = $lname;
+                # set a f3 variable to the input
+                $f3->set("lname", $lname);
+                return true;
+            } # input is not alphabetic, set an f3 to appropriate error
+            else {
+                $f3->set("errors['lname']", "Last name must be alphabetic");
+            }
+        } # input is empty, set an f3 variable to appropriate error
         else {
-            $f3->set("errors['lname']", "Last name must be alphabetic");
+            $f3->set("errors['lname']", "Last name is required");
         }
-    } # input is empty, set an f3 variable to appropriate error
-    else {
-        $f3->set("errors['lname']", "Last name is required");
     }
-}
 
 # Function to validate the age
-function validAge($age, $f3) {
-    # check if the input is not empty and numeric
-    if (!empty($age) && is_numeric($age)) {
-        # check if age is in range(18-118)
-        if (($age >= 18) && ($age <= 118)) {
-            # set a session variable to the input
-            //$_SESSION['age'] = $age;
-            # set a f3 variable to the input
-            $f3->set("age", $age);
-            return true;
-        } # valid number but not in range
+    function validAge($age, $f3) {
+        # check if the input is not empty and numeric
+        if (!empty($age) && is_numeric($age)) {
+            # check if age is in range(18-118)
+            if (($age >= 18) && ($age <= 118)) {
+                # set a session variable to the input
+                //$_SESSION['age'] = $age;
+                # set a f3 variable to the input
+                $f3->set("age", $age);
+                return true;
+            } # valid number but not in range
+            else {
+                $f3->set("errors['age']", "Age must be 18-118");
+            }
+        } # user did not enter a valid number
         else {
-            $f3->set("errors['age']", "Age must be 18-118");
+            $f3->set("errors['age']", "Please enter a valid age");
         }
-    } # user did not enter a valid number
-    else {
-        $f3->set("errors['age']", "Please enter a valid age");
-    }
 
-}
+    }
 
 # Function to validate phone
-function validPhone($num, $f3) {
-    if (!empty($num)) {
-        if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $num)) {
-            # set a session variable to the input
-            //$_SESSION['phone'] = $num;
-            # set a f3 variable to the input
-            $f3->set("phone", $num);
-            return true;
-        }
+    function validPhone($num, $f3) {
+        if (!empty($num)) {
+            if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $num)) {
+                # set a session variable to the input
+                //$_SESSION['phone'] = $num;
+                # set a f3 variable to the input
+                $f3->set("phone", $num);
+                return true;
+            }
+            else {
+                $f3->set("errors['phone']",  "Enter in this format: 000-000-0000");
+                $f3->set("phone", $num);
+            }
+        } # user did not enter a valid number
         else {
-            $f3->set("errors['phone']",  "Enter in this format: 000-000-0000");
-            $f3->set("phone", $num);
+            $f3->set("errors['phone']", "Please enter a phone number");
         }
-    } # user did not enter a valid number
-    else {
-        $f3->set("errors['phone']", "Please enter a phone number");
     }
-}
 
 // function to validate the email
-function validEmail($email, $f3) {
-    # if entered email matches the right format
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        # set a session variable to the input
-        //$_SESSION['email'] = $email;
-        # set a f3 variable to the input
-        $f3->set("email", $email);
-        return true;
-    } # not a valid email
-    else {
-        $f3->set("email", $email);
-        $f3->set("errors['email']", "Please enter a valid email");
+    function validEmail($email, $f3) {
+        # if entered email matches the right format
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            # set a session variable to the input
+            //$_SESSION['email'] = $email;
+            # set a f3 variable to the input
+            $f3->set("email", $email);
+            return true;
+        } # not a valid email
+        else {
+            $f3->set("email", $email);
+            $f3->set("errors['email']", "Please enter a valid email");
+        }
     }
-}
 
 // function to validate outdoor options
-function validOutdoor($outArray, $f3, $array, $obj) {
-    $isValid = true;
+    function validOutdoor($outArray, $f3, $array, $obj) {
+        $isValid = true;
 
-    $_SESSION['outdoor'] = array();
-    if (isset($outArray)) {
-        foreach ($outArray as $item) {
-            if (in_array($item , $array)) {
-                # set a session variable to the input
-                //array_push( $_SESSION['outdoor'], $item);
-                # pass the item to the member class
-                $obj->setOutdoor($item);
-            }
-            else {
-                $f3->set("errors['outdoor']", "Not a valid option");
-                $isValid = false;
-            }
+        $_SESSION['outdoor'] = array();
+        if (isset($outArray)) {
+            foreach ($outArray as $item) {
+                if (in_array($item , $array)) {
+                    # set a session variable to the input
+                    //array_push( $_SESSION['outdoor'], $item);
+                    # pass the item to the member class
+                    $obj->setOutdoor($item);
+                }
+                else {
+                    $f3->set("errors['outdoor']", "Not a valid option");
+                    $isValid = false;
+                }
 
+            }
         }
-    }
 
-    return $isValid;
-}
+        return $isValid;
+    }
 
 // function to validate indoor options
-function validIndoor($inArray, $f3, $array, $obj) {
-    $isValid = true;
+    function validIndoor($inArray, $f3, $array, $obj) {
+        $isValid = true;
 
-    $_SESSION['indoor'] = array();
-    if (isset($inArray)) {
-        foreach ($inArray as $item) {
-            if (in_array($item , $array)) {
-                # set a session variable to the input
-                //array_push( $_SESSION['indoor'], $item);
-                # pass the item to the member class
-                $obj->setIndoor($item);
-            }
-            else {
-                $f3->set("errors['indoor']", "Not a valid option");
-                $isValid = false;
-            }
+        $_SESSION['indoor'] = array();
+        if (isset($inArray)) {
+            foreach ($inArray as $item) {
+                if (in_array($item , $array)) {
+                    # set a session variable to the input
+                    //array_push( $_SESSION['indoor'], $item);
+                    # pass the item to the member class
+                    $obj->setIndoor($item);
+                }
+                else {
+                    $f3->set("errors['indoor']", "Not a valid option");
+                    $isValid = false;
+                }
 
+            }
         }
+        return $isValid;
     }
-    return $isValid;
 }
+
+
